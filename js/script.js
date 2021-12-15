d16bb66d93e82482fdf733c1d781c09605a00ab2
Yunus <seytnazarov.yunus@gmail.com>
2021-12-16 00:19:38 +0300

    Flat 31 Updates

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-const flatRealize = [
-  [0, 1],
-  [2, 3],
-  [4, 5],
-].reduce((a, b) => a.concat(b));
-console.log(flatRealize);
-
 const arr1 = [1, 2, 3, [1, 2, 3, 4, [2, 3, 4]]];
 const arr2 = [1, 2, 3, 4, 5];
 function flatten(input) {
@@ -32,3 +25,35 @@ function flatten(input) {
 console.log(flatten(arr1));
 // [1, 2, 3, 1, 2, 3, 4, 2, 3, 4]
 //[1, 2, 3, 4, 5]
+
+const flatRealize = [
+  [0, 1],
+  [2, 3],
+  [4, 5],
+].reduce((a, b) => a.concat(b));
+console.log(flatRealize);
+
+const flatSingle = (arr) => [].concat(...arr);
+console.log(flatSingle([1, 2, 3, [1, 2, 3, 4, 2, 3, 4]]));
+
+const arr3 = [1, 2, 3, [1, 2, 3, 4, [2, 3, 4]]];
+function flattenSecond(array) {
+  const flattend = [];
+  if (arguments.length > 1) {
+    throw new Error(
+      "Function accepts only 1 argument, too much arguments provided"
+    );
+  }
+  (function flat(array) {
+    array.forEach(function (el) {
+      if (Array.isArray(el)) {
+        flat(el);
+      } else {
+        flattend.push(el);
+      }
+    });
+  })(array);
+  return flattend;
+}
+
+console.log(flattenSecond(arr3));
